26e03202bfa6e1d36d60a99c604af645cc368ebd
Andreas19 <andreas19@posteo.eu>
2018-12-25 16:47:53 +0100

    Add function to module inputs

--- src/salmagundi/inputs.py
+++ src/salmagundi/inputs.py
@@ -6,7 +6,11 @@ The terminal must support `ANSI escape sequences
 .. versionadded:: 0.3.0
 """
 
-from ansictrls import erase, EraseMode, save_pos, move
+from functools import partial
+
+from ansictrls import CS
+
+_cs_print = partial(print, end='', sep='', flush=True)
 
 
 def line(prompt='', default=None, check=None, exc_on_cancel=False):
@@ -38,19 +42,16 @@ def line(prompt='', default=None, check=None, exc_on_cancel=False):
     :rtype: str or return-type of the ``check`` callable or None
     :raises EOFError: if input was cancelled and ``exc_on_cancel=True``
     :raises TypeError: if ``default`` is not of type ``str``
-
-    .. versionadded:: 0.3.0
     """
     if default is not None and not isinstance(default, str):
         raise TypeError('default value %r not of type str' % default)
     value = None
-    print()
-    move(-1)
+    _cs_print('\n', CS.CUU, CS.SCP)
     while True:
         try:
-            with save_pos():
-                a = input(prompt) or default
+            a = input(prompt) or default
         except EOFError:
+            print()
             if exc_on_cancel:
                 raise
             break
@@ -64,8 +65,7 @@ def line(prompt='', default=None, check=None, exc_on_cancel=False):
             else:
                 value = a
                 break
-        erase(EraseMode.SCRN_END)
-    print()
+        _cs_print(CS.RCP, CS.ED)
     return value
 
 
@@ -91,8 +91,6 @@ def yesno(prompt, yesno, exc_on_cancel=False):
     :raises EOFError: if input was cancelled and ``exc_on_cancel=True``
     :raises TypeError: if ``yesno`` is not of type ``str``
     :raises ValueError: if ``yesno`` is not of length 2
-
-    .. versionadded:: 0.3.0
     """
     if not isinstance(yesno, str):
         raise TypeError('yesno argument %r not of type str' % yesno)
@@ -109,8 +107,62 @@ def yesno(prompt, yesno, exc_on_cancel=False):
         s1 = s.lower()
         s2 = yesno.lower()
         if s1 not in s2:
-            raise ValueError()
+            raise ValueError
         return s1 == s2[0]
 
     return line('%s [%s%s] ' % (prompt, *yesno), default=default, check=f,
                 exc_on_cancel=exc_on_cancel)
+
+
+def select(prompt, options, default=None, case_sensitive=False,
+           exc_on_cancel=False):
+    r"""Show an input with selectable options.
+
+    >>> select('Select: [T]op, [B]ottom, [L]eft, [R]ight > ', 'TBLR')
+    Select: [T]op, [B]ottom, [L]eft, [R]ight > b
+    1
+
+    Simple menus can be created like this:
+
+    >>> titles = ('Number 1', 'Number 2', 'Num 3', 'Numero 42', 'Number 5')
+    >>> options = range(1, 6)
+    >>> lines = (t.ljust(11, '.') + '[%02d]' % o for t, o in zip(titles, options))
+    >>> select('\n'.join(lines) + '\n-> ', ''.join(map(str, options)))
+    Number 1...[01]
+    Number 2...[02]
+    Num 3......[03]
+    Numero 42..[04]
+    Number 5...[05]
+    -> 4
+    3
+
+    :param str prompt: the prompt
+    :param options: the options; if all options are only 1 character a string
+                    can be used, else a tuple of strings
+    :type options: str or tuple
+    :param default: default value that will be used if no input is provided
+    :type default: str or None
+    :param bool case_sensitive: if ``False`` case of typed characters will
+                                be ignored
+    :param bool exc_on_cancel: if set to ``True`` an EOF will cause an Exception
+    :return: index of the selected option in ``options`` or None if cancelled
+             and ``exc_on_cancel=False``
+    :rtype: bool or None
+    :raises EOFError: if input was cancelled and ``exc_on_cancel=True``
+    :raises TypeError: if ``options`` is not str or tuple of strings
+
+    .. versionadded:: 0.4.0
+    """
+    if not isinstance(options, (str, tuple)):
+        raise TypeError('argument options must be of type str or tuple')
+    if not case_sensitive:
+        options = tuple(map(str.lower, options))
+
+    def f(s):
+        if not case_sensitive:
+            s = s.lower()
+        if s not in options:
+            raise ValueError
+        return options.index(s)
+
+    return line(prompt, default=default, check=f, exc_on_cancel=exc_on_cancel)
