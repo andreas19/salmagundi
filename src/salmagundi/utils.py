20b1589480cb06049de14f24f1775ca071efc23c
Andreas19 <andreas19@posteo.eu>
2019-01-07 11:03:50 +0100

    Add function to module utils

--- src/salmagundi/utils.py
+++ src/salmagundi/utils.py
@@ -6,7 +6,7 @@
 import os
 
 
-def check_type(obj, classinfo, name='object', alt_msg=None):
+def check_type(obj, classinfo, name='object', msg=None):
     """Check the type of an object.
 
     >>> utils.check_type(1, str, 'num')
@@ -17,7 +17,7 @@ def check_type(obj, classinfo, name='object', alt_msg=None):
     Traceback (most recent call last):
       ...
     TypeError: the type of 'num' must be one of 'str, float', got 'int'
-    >>> utils.check_type(1, str, 'num', 'wrong type for num')
+    >>> utils.check_type(1, str, msg='wrong type for num')
     Traceback (most recent call last):
       ...
     TypeError: wrong type for num
@@ -25,33 +25,47 @@ def check_type(obj, classinfo, name='object', alt_msg=None):
     :param object obj: the object
     :param classinfo: see :func:`isinstance`
     :param str name: name shown in the exception message
-    :param str alt_msg: alternative message
+    :param str msg: message for the exception
     :raises TypeError: if the check fails
     """
     if not isinstance(obj, classinfo):
-        if alt_msg:
-            msg = alt_msg
-        elif isinstance(classinfo, tuple):
-            msg = ('the type of %r must be one of %r, got %r' %
-                   (name, ', '.join(map(lambda x: x.__name__, classinfo)),
-                    obj.__class__.__name__))
-        else:
-            msg = ('the type of %r must be %r, got %r' %
-                   (name, classinfo.__name__, obj.__class__.__name__))
+        if not msg:
+            if isinstance(classinfo, tuple):
+                msg = ('the type of %r must be one of %r, got %r' %
+                       (name, ', '.join(map(lambda x: x.__name__, classinfo)),
+                        obj.__class__.__name__))
+            else:
+                msg = ('the type of %r must be %r, got %r' %
+                       (name, classinfo.__name__, obj.__class__.__name__))
         raise TypeError(msg)
 
 
-def check_path_like(obj, name='object', alt_msg=None):
+def check_path_like(obj, name='object', msg=None):
     """Check if an object is path-like.
 
     :param object obj: the object
     :param str name: name shown in the exception message
-    :param str alt_msg: alternative message
+    :param str msg: alternative message
     :raises TypeError: if the check fails
     """
-    if alt_msg:
-        msg = alt_msg
-    else:
+    if not msg:
         msg = ('%r must be a path-like object, got %r' %
                (name, obj.__class__.__name__))
     check_type(obj, (str, bytes, os.PathLike), None, msg)
+
+
+def check_bytes_like(obj, name='object', msg=None):
+    """Check if an object is bytes-like.
+
+    :param object obj: the object
+    :param str name: name shown in the exception message
+    :param str msg: alternative message
+    :raises TypeError: if the check fails
+    """
+    if not msg:
+        msg = ('%r must be a bytes-like object, got %r' %
+               (name, obj.__class__.__name__))
+    try:
+        memoryview(obj)
+    except TypeError:
+        raise TypeError(msg) from None
